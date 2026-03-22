4fd9a9c891370526e9a76d70a36c7de4ac108961
Blare <m13m40m@gmail.com>
2026-03-22 22:39:50 +0330

    add run code for server and client

---
+++ run_client.py
@@ -0,0 +1,8 @@
+from client.gui import UDPClientGUI
+
+def main():
+    app = UDPClientGUI()
+    app.run()
+
+if __name__ == "__main__":
+    main()
---
+++ run_server.py
@@ -0,0 +1,8 @@
+from server.gui import UDPRelayGUI
+
+def main():
+    app = UDPRelayGUI()
+    app.run()
+
+if __name__ == "__main__":
+    main()
